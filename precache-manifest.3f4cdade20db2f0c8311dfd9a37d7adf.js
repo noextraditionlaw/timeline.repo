a24c0ff857c327d9d86db134d1e72733b74aaf26
go.rub.that.lamp <go.rub.that.lamp@gmail.com>
2019-08-20 00:35:48 +0800

    Updates

---
+++ precache-manifest.3f4cdade20db2f0c8311dfd9a37d7adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e830f934f4b640253791d6a4623e8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80cb5df2a0f53d062e27",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "a149159955365e913661",
+    "url": "/static/js/2.cd9c97b1.chunk.js"
+  },
+  {
+    "revision": "80cb5df2a0f53d062e27",
+    "url": "/static/js/main.15ca63a1.chunk.js"
+  },
+  {
+    "revision": "e1ea0a0756b348f22361",
+    "url": "/static/js/runtime~main.d9c13f3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
